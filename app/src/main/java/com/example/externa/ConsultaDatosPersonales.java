7e95fb228e7f722884f9ef2bcad73d0c07573397
Diego99o <=>
2020-05-26 22:13:49 -0500

    hoy

--- app/src/main/java/com/example/externa/ConsultaDatosPersonales.java
+++ app/src/main/java/com/example/externa/ConsultaDatosPersonales.java
@@ -11,15 +11,17 @@ public class ConsultaDatosPersonales extends StringRequest {
     private static final String REGISTER_REQUEST_URL = "https://www.redesoft.com.co/Info_Antigua/ConsultaDatosPersonales.php";
     private Map<String, String> params;
 
-    public ConsultaDatosPersonales(int id_cliente,String tipo_doc, int cedula, String genero, String estado, String ciudad_residencia,String dir, String ciudad_nacimiento,int telefono_fijo, Response.Listener<String> listener, Response.ErrorListener error)
+    public ConsultaDatosPersonales(String Nomcli, String Apecli, int Edadcli, int Telecli,String tipo_doc, int cedula, String genero, String ciudad_residencia,String dir, String ciudad_nacimiento,int telefono_fijo, Response.Listener<String> listener, Response.ErrorListener error)
     {
         super(Request.Method.POST, REGISTER_REQUEST_URL, listener, error);
         params = new HashMap<>();
-        params.put("id_cliente", id_cliente+"");
+        params.put("Nomcli", Nomcli);
+        params.put("Apecli", Apecli);
+        params.put("Edadcli", Edadcli+"");
+        params.put("Telecli", Telecli+"");
         params.put("tipo_doc", tipo_doc);
         params.put("cedula", cedula+"");
         params.put("genero", genero);
-        params.put("estado", estado);
         params.put("ciudad_residencia", ciudad_residencia);
         params.put("dir", dir);
         params.put("ciudad_nacimiento", ciudad_nacimiento);
--- app/src/main/java/com/example/externa/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/example/externa/ui/dashboard/DashboardFragment.java
@@ -1,6 +1,8 @@
 package com.example.externa.ui.dashboard;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,7 +14,16 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.Volley;
+import com.example.externa.Bienvenidos;
+import com.example.externa.ConsultaDatosPersonales;
 import com.example.externa.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class DashboardFragment extends Fragment {
 
@@ -30,6 +41,47 @@ public class DashboardFragment extends Fragment {
 //                textView.setText(s);
 //            }
 //        });
+        Llenarvista();
         return root;
+    }
+    private void Llenarvista(){
+
+        Response.Listener<String> responListener = new Response<>().Listener<String>() {
+            @Override
+            public void onResponse(String response)
+            {
+                try {
+                    JSONObject jsonResponse = new JSONObject(response);
+                    boolean success = jsonResponse.getBoolean("success");
+                    String id = jsonResponse.getString("Idcli");
+                    //Toast.makeText(this, "El cédula o contraseña es incorrecta.", Toast.LENGTH_LONG).show();
+
+                    if (success)
+                    {
+
+                    }
+                    else
+                    {
+                        AlertDialog.Builder hh = new AlertDialog.Builder(getActivity());
+                        hh.setMessage("No hay datos del usuario")
+                                .setNegativeButton("Reintentar",null)
+                                .create().show();
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        Response.ErrorListener error = new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("ERROR REGISTRO", error.toString());
+            }
+        };
+
+        ConsultaDatosPersonales dat= new ConsultaDatosPersonales(Nomcli, Apecli,Edadcli, Telecli,tipo_doc,cedula,genero, ciudad_residencia,dir, ciudad_nacimiento,telefono_fijo, responListener, error);
+        RequestQueue queue= Volley.newRequestQueue(getActivity().getApplicationContext());
+        queue.add(dat);
     }
 }
